70dcef40f73d4422ebac5ac0e9bb237e33aa2e21
donzazi <ellyas20@live.dk>
2025-04-24 11:18:31 +0200

    Update - read temperature

--- arduino/arduino.ino
+++ arduino/arduino.ino
@@ -1,10 +1,20 @@
 //TEST BITCHES!!!
 void setup() {
   // put your setup code here, to run once:
+    Serial.begin(9600);
+    while(!Serial);
+    carrier.noCase();
+    carrier.begin();
+    delay(2000);
 
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  float temperature = carrier.Env.readTemperature();
+    Serial.print("Temperatur: ");
+    Serial.print(temperature);
+    Serial.println(" °C");
 
+     delay(5000);
 }
