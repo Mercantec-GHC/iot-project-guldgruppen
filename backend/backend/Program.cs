37f5b9ef675fcbda68e9659c6bd6090b65e95796
Mikkel konyher <146730338+mikkelkonyher@users.noreply.github.com>
2025-04-24 11:24:14 +0200

    PIR-sensor

--- backend/backend/Program.cs
+++ backend/backend/Program.cs
@@ -9,12 +9,12 @@ builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+// Enable CORS
+app.UseCors();
+
+app.UseSwagger();
+
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 
