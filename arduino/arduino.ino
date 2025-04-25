62075082bdf27f3e499591162f3b9d9df3c77d12
rasmussckaletz <rasmus@leeloo.dk>
2025-04-25 10:43:19 +0200

    seperated sensor files, added functions to all sensors and read them in arduino.ino (main file)

--- arduino/arduino.ino
+++ arduino/arduino.ino
@@ -1,10 +1,27 @@
-//TEST BITCHES!!!
-void setup() {
-  // put your setup code here, to run once:
+#include <Arduino_MKRIoTCarrier.h>
 
+MKRIoTCarrier carrier;
+
+int pir = A5;
+bool screenActive = false;
+unsigned long screenOnTime = 0;  // Time when screen was activated
+const unsigned long screenDuration = 30000; // 30 seconds
+
+void setup() {
+  Serial.begin(9600);
+  while (!Serial);  // Wait for Serial Monitor
+
+  carrier.noCase();
+  carrier.begin();
+
+  pinMode(pir, INPUT);
+
+  carrier.display.setRotation(0);
+  carrier.display.fillScreen(0x0000); // Start with screen off
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  getPIRSensorRead();
+  getMoistureSensorRead();
+  getTemperaturSensorRead();
 }
