420a9bee0f9d94cae7e089d7fc8242ee50bc8d27
Rasmus Sckaletz <rasmus@leeloo.dk>
2025-05-06 13:22:37 +0200

    sms service works through api, needs to fix on react (button doesnt work)

--- react/src/SMSTest.jsx
+++ react/src/SMSTest.jsx
@@ -2,11 +2,24 @@ import React from 'react'
 
 function SmsTest() {
 
+    const sendSms = async () => {
+        await fetch('/api/sms/send', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                to: '+4555210815',
+                message: 'Hello from Twilio!',
+            }),
+        });
+    };
+
     return (
         <div>
             <h1>Test SMS</h1>
             <p>This is a test for sending SMS.</p>
-            {/*<button onClick={sendSms}>Send Test SMS</button>*/}
+            <button onClick={sendSms}>Send Test SMS</button>
         </div>
     );
 }
