1f59eadbd1ffaeedf9b9ab4d2600bf55c6bf8d6f
Mikkel konyher <146730338+mikkelkonyher@users.noreply.github.com>
2025-05-21 11:08:13 +0200

    Update AuthContext.jsx

--- react/src/AuthContext.jsx
+++ react/src/AuthContext.jsx
@@ -1,16 +1,20 @@
 import { createContext, useContext, useState, useEffect } from 'react';
 
+// Opretter en ny Context til autentificering
 const AuthContext = createContext();
 
 export const AuthProvider = ({ children }) => {
+    // Opretter en ny Context til autentificering
     const [isAuthenticated, setIsAuthenticated] = useState(null);
 
     useEffect(() => {
+        // Tjekker, om der findes en token i localStorage ved første render
         const token = localStorage.getItem('token');
         setIsAuthenticated(!!token);
     }, []);
 
     const logout = () => {
+        // Logger brugeren ud ved at nulstille state og fjerne token
         setIsAuthenticated(false);
         localStorage.removeItem('token');
     };
